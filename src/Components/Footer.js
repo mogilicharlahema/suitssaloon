72a1efe3202e76a7ed46238b07664f3b6d250ba9
Hema Mogilicharla <hema@surveyheart.com>
2025-05-05 17:58:55 +0530

    changes completed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -23,7 +23,7 @@ const FooterSection = () => {
                             <a href="https://www.instagram.com/thesuitssalon?igsh=aGNiZDd4cmZ3Ynkz" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
                                <img src={instagramIcon} alt="Instagram" />
                             </a>
-                            <a href="https://google.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+                            <a href="https://g.co/kgs/1KFCsqn" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
                                <img src={facebookIcon} alt="Facebook" />
                             </a>
                             <a href="https://www.facebook.com/profile.php?id=61561106905978" target="_blank" rel="noopener noreferrer" aria-label="Google">
--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,5 +1,6 @@
 import React from "react";
-import hrimage from "../Assets/hrimage.png";
+import heroimage from "../Assets/heroimage.png";
+import herocontent from "../Assets/herocontent.png";
 import "../Styles/HeroSection.css";
 import { useNavigate } from "react-router-dom";
 
@@ -11,7 +12,8 @@ const HeroSectionPage = () => {
   return (
     <>
       <div className="hero-container">
-        <img className="hero-pic" src={hrimage} alt="hero-image" />
+        <img className="hero-pic" src={heroimage} alt="hero-image" />
+        <img className="hero-content" src={herocontent} alt="hero-content" />
         <button className="hero-book-btn" onClick={handleHeroBookBtn}>Book an Appointment</button>
       </div>
       <br></br>
